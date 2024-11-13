20fabd9183a6143e613cc54b374d234dd0117f7e
РОдион <kuzminrt@gmail.com>
2024-11-13 18:01:19 +0300

    начал делать сраницу регистрации

--- users/forms.py
+++ users/forms.py
@@ -8,3 +8,19 @@ class CustomUserCreationForm(UserCreationForm):
     height = forms.FloatField(required=True, label="Рост (см)")
     gender = forms.ChoiceField(choices=[("male", "Мужской"),("female", "Женский")],required=True, label="Пол" )
     birthdate = forms.DateField(required=True, label="Дата рождения")
+
+
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('username', 'email', 'weight', 'height', 'gender', 'birth_date')
+
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.weight = self.cleaned_data.get('weight')
+        user.height = self.cleaned_data.get('height')
+        user.gender = self.cleaned_data.get('gender')
+        user.birth_date = self.cleaned_data.get('birth_date')
+        if commit:
+            user.save()
+        return user
