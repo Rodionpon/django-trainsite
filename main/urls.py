f11c139543192522e629c7216d6eba9624d01817
РОдион <kuzminrt@gmail.com>
2025-01-19 22:36:47 +0300

    создал стр для создания тренировки и еще раного

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path 
-from .views import dashboard
+from .views import dashboard, trainings, TrainingListView, TrainingCreateView
+
 
 app_name = "main"
 urlpatterns = [
     path("", dashboard, name="dashboard"),
+    path("trainigs/", TrainingListView.as_view(), name="trainings"),
+    path("create_training/", TrainingCreateView.as_view(), name="create_treaning")
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, CreateView
+from .models import TrainingProdram, Training
 # Create your views here.
 
 # @login_required
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+def trainings(request):
+    return render(request, 'trainings.html')
+
+class TrainingListView(ListView):
+    model = Training
+    template_name = "trainings.html"
+    context_object_name = "trainings"
+
+    def get_queryset(self):
+        return Training.objects.all()
+    
+class TrainingCreateView(CreateView):
+    model = Training
+    template_name = "create_training.html"
+    fields = ["title","description","image"]
+    success_url = reverse_lazy("main:trainings")
